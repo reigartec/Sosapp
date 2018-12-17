075683f7b5a7e10841b921edfcf3883e91d2019f
Reigartec <Reinaldo788@gmail.com>
2018-12-16 23:29:24 -0400

    A trabajar con los botones dentro de la solicitud.

--- app/src/main/java/ado/edu/itla/sosapp/InicioActivity.java
+++ app/src/main/java/ado/edu/itla/sosapp/InicioActivity.java
@@ -2,7 +2,9 @@ package ado.edu.itla.sosapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
@@ -26,7 +28,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ado.edu.itla.sosapp.entidad.Solicitud;
+import ado.edu.itla.sosapp.entidad.Usuario;
+import ado.edu.itla.sosapp.repositorio.funciones.Sesion;
 import ado.edu.itla.sosapp.repositorio.solicitud.SolicitudRepositorioimpl;
+import ado.edu.itla.sosapp.repositorio.usuario.UsuarioRepositorioimpl;
 
 public class InicioActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -57,7 +62,7 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
         NavigationView navigationView = findViewById(R.id.menuview);
         navigationView.setNavigationItemSelectedListener(this);
 
-        seleccionarItem(R.id.action_inicio);
+        //seleccionarItem(R.id.action_inicio);
 
         /**************TRABAJANDO CON LISTAS********************/
         list = findViewById(R.id.missolicitudes_list);
@@ -76,7 +81,6 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
                         "Crear vista solicitud para la solicitud "+String.valueOf(solicituds.get(position).getId())+
                         ", de la posición ("+position+")",Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(InicioActivity.this, VerSolicitud.class);
-                String idSolicitud = null;
                 i.putExtra("idSolicitud", String.valueOf(solicituds.get(position).getId()));
                 startActivity(i);
             }
@@ -102,24 +106,45 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
         if(itemid == R.id.action_inicio)
         {
             titulo = titulo + " - Inicio";
+            /**************TRABAJANDO CON LISTAS********************/
+            list = findViewById(R.id.missolicitudes_list);
+            final List<Solicitud> solicituds = new SolicitudRepositorioimpl(getApplicationContext()).buscarTodos();
+            SolicitudAdapter adapter = new SolicitudAdapter(getApplicationContext(),solicituds);
+            list.setAdapter(adapter);
+            /**************TRABAJANDO CON LISTAS********************/
         }else if(itemid == R.id.action_missolicitudes)
         {
             titulo = titulo + " - Mis solicitudes";
-        }else if(itemid == R.id.action_solicitudes)
+
+            /**************TRABAJANDO CON LISTAS********************/
+            list = findViewById(R.id.missolicitudes_list);
+            Sesion sesion = new Sesion(getApplicationContext());
+            String correo = sesion.get("email");
+            Usuario usuario = new UsuarioRepositorioimpl(getApplicationContext()).buscar(correo);
+            final List<Solicitud> solicituds = new SolicitudRepositorioimpl(getApplicationContext()).buscarSolicitudesPor(usuario);
+            SolicitudAdapter adapter = new SolicitudAdapter(getApplicationContext(),solicituds);
+            list.setAdapter(adapter);
+            /**************TRABAJANDO CON LISTAS********************/
+
+        }/*else if(itemid == R.id.action_solicitudes)
         {
             titulo = titulo + " - Solicitudes";
             it = new Intent(InicioActivity.this, Solicitudes.class);
-        }else if(itemid == R.id.action_nsolicitud)
+        }*/else if(itemid == R.id.action_nsolicitud)
         {
             titulo = titulo + " - Nueva Solicitud";
             it = new Intent(InicioActivity.this, NuevaSolicitud.class);
         }
         else if(itemid == R.id.action_settings)
         {
-            titulo = titulo + " - Configuración";
+            titulo = titulo + " - Configuración \n En construcción!!!";
         }else if(itemid == R.id.action_Cerrars)
         {
             //Cerrar sesión
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            preferences.edit().remove("email").commit();
+            it = new Intent(InicioActivity.this,MainActivity.class);
+
         }
 
         if(it != null)
--- app/src/main/java/ado/edu/itla/sosapp/VerSolicitud.java
+++ app/src/main/java/ado/edu/itla/sosapp/VerSolicitud.java
@@ -1,23 +1,34 @@
 package ado.edu.itla.sosapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import ado.edu.itla.sosapp.entidad.Solicitud;
+import ado.edu.itla.sosapp.repositorio.Dbconexion;
 import ado.edu.itla.sosapp.repositorio.funciones.Sesion;
 import ado.edu.itla.sosapp.repositorio.solicitud.SolicitudRepositorioimpl;
 
 public class VerSolicitud extends AppCompatActivity {
     Sesion sesion = null;
+    Solicitud solicitud=null;
+    String correo="";
+    private Dbconexion dbConexion;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ver_solicitud);
         String idSolicitud="";
+        dbConexion = new Dbconexion(getApplicationContext());
         if (savedInstanceState == null) {
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
@@ -32,9 +43,11 @@ public class VerSolicitud extends AppCompatActivity {
                 "Solicitud "+idSolicitud+
                         ", Lista para ser visualizada ",Toast.LENGTH_SHORT).show();
         sesion = new Sesion(getApplicationContext());
-        String correo = sesion.get("email");
+        correo = sesion.get("email");
         int id =  Integer.parseInt(idSolicitud);
-        Solicitud solicitud = new SolicitudRepositorioimpl(getApplicationContext()).buscarPor(id);
+        solicitud = new SolicitudRepositorioimpl(getApplicationContext()).buscarPor(id);
+        final Button bcancelar = findViewById(R.id.solicitud_boton);
+        Button boton2 = findViewById(R.id.solicitud_boton2);
 
         TextView titulo = findViewById(R.id.solicitud_lstitulo);
         titulo.setText(solicitud.getTitulo());
@@ -48,27 +61,110 @@ public class VerSolicitud extends AppCompatActivity {
         TextView ttc = findViewById(R.id.labelcreatra);
         TextView qtra = findViewById(R.id.solicitud_lsusuario);
 
-        if(solicitud.getUsuarioSolicitante().getEmail().equals(correo))
+        if(solicitud.getUsuarioSolicitante().getEmail().toLowerCase().equals(correo.toLowerCase()))
         {
             ttc.setText("Trabajando por:");
-            
-        }else
-            {
-                ttc.setText("Creado por:");
-            }
-            try {
+            boton2.setVisibility(View.INVISIBLE);
+            //bcancelar.setText("");
 
+            try {
                 if (solicitud.getUsuarioAsignado().getId() != 0) {
                     qtra.setText(solicitud.getUsuarioAsignado().getNombre());
                 } else {
                     qtra.setText("Nadie interesado");
                 }
+
             }catch (Exception e)
             {
                 qtra.setText("Nadie interesado ");
                 Log.i("Error",e.getMessage());
             }
 
+        }else
+            {
+                bcancelar.setText("Seleccionar");
+                boton2.setVisibility(View.INVISIBLE);
+                ttc.setText("Creado por:");
+                qtra.setText(solicitud.getUsuarioSolicitante().getNombre());
+            }
+
+
+            bcancelar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(getApplicationContext(),
+                      //      "Afuera - "+solicitud.getUsuarioSolicitante().getEmail()+
+                        //    " - correo - "+correo,Toast.LENGTH_SHORT).show();
+                    if(solicitud.getUsuarioSolicitante().getEmail().toLowerCase().equals(correo.toLowerCase()))
+                    {
+                        AlertDialog cajaopcion = pregunta(solicitud.getId());
+                        cajaopcion.show();//Opcion para borrar la solicitud o no.
+                        //Toast.makeText(getApplicationContext(),
+                          //      "Solicitud prueba click cancelar ",Toast.LENGTH_SHORT).show();
+
+                    }else
+                    {
+                            if(bcancelar.getText().toString().equals("Seleccionar"))
+                            {
+                                Toast.makeText(getApplicationContext(),
+                                        "Probando, si funciona",Toast.LENGTH_SHORT).show();
+                            }
+                    }
+                }
+            });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+
+    private AlertDialog pregunta(final int id)
+    {
+        AlertDialog mDialogBox =new AlertDialog.Builder(this)
+                //set message, title, and icon
+                .setTitle("Borrar Solicitud")
+                .setMessage("Seguro que quieres borrar tu solicitud?")
+                .setIcon(R.drawable.ic_menu_slideshow)
+
+                .setPositiveButton("Borrar", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Codigo para borrar
+                        SQLiteDatabase db = dbConexion.getWritableDatabase();
+
+                        db.delete("solicitud","id=?",new String[]{String.valueOf(id)});
+
+                        Log.e("TAG_BORRAR","Borrando la solicitud");
+
+                        db.close(); // Closing database connection
+                        //solicitud.
+                        dialog.dismiss();
+                        //onBackPressed();
+                        Intent i = new Intent(VerSolicitud.this, InicioActivity.class);
+                        startActivity(i);
+                        Toast.makeText(getApplicationContext(),
+                              "Solicitud borrada",Toast.LENGTH_SHORT).show();
+                    }
+
+                })
+
+
+
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+
+                    }
+                })
+                .create();
+        return mDialogBox;
 
     }
 
